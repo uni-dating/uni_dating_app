eaaafb859ae4b0aa32e1cc3891cc3aa1b33d0333
KhudoliiAndrew <khudolii.a.s@gmail.com>
2022-07-15 08:24:33 +0200

    dev: removed back email verification.

--- lib/ui/resources/dimens.dart
+++ lib/ui/resources/dimens.dart
@@ -3,6 +3,6 @@ class Dimens {
   static Duration fastDuration = const Duration(milliseconds: 150);
   static Duration delayAfterGameCompleted = const Duration(seconds: 2);
 
-  //static String endEmail = '@student.reutlingen-university.de';
-  static String endEmail = '1';
+  static String endEmail = '@student.reutlingen-university.de';
+  //static String endEmail = '1';
 }
